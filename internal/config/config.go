4fafd915d6a4a01b4200246d4448c95f1892bf5a
simonlewi <simon.wilen@gmail.com>
2025-04-20 16:33:50 +0200

    Fix json config, reading from user home directory

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,13 +1,17 @@
-package main
+package config
 
 import (
+	"encoding/json"
 	"os"
+	"path/filepath"
 )
 
 type Config struct {
 	DBUrl           string `json:"db_url"`
 	CurrentUsername string `json:"current_username"`
 }
+
+const configFileName = ".gatorconfig.json"
 
 func NewConfig(dbUrl, username string) *Config {
 	return &Config{
@@ -21,5 +25,50 @@ func getConfigFilePath() (string, error) {
 	if err != nil {
 		return "", err
 	}
-	return homeDir + "/.gatorconfig.json", nil
+	return filepath.Join(homeDir, configFileName), nil
+}
+
+func write(cfg Config) error {
+	configPath, err := getConfigFilePath()
+	if err != nil {
+		return err
+	}
+
+	jsonData, err := json.Marshal(cfg)
+	if err != nil {
+		return err
+	}
+
+	err = os.WriteFile(configPath, jsonData, 0644)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func Read() (Config, error) {
+	var cfg Config
+
+	configPath, err := getConfigFilePath()
+	if err != nil {
+		return cfg, err
+	}
+
+	fileData, err := os.ReadFile(configPath)
+	if err != nil {
+		return cfg, err
+	}
+
+	err = json.Unmarshal(fileData, &cfg)
+	if err != nil {
+		return cfg, err
+	}
+
+	return cfg, nil
+}
+
+func (c *Config) SetUser(username string) error {
+	c.CurrentUsername = username
+	return write(*c)
 }
--- main.go
+++ main.go
@@ -1,1 +1,29 @@
 package main
+
+import (
+	"fmt"
+
+	"github.com/simonlewi/gator/internal/config"
+)
+
+func main() {
+	cfg, err := config.Read()
+	if err != nil {
+		fmt.Println("Error reading config:", err)
+		return
+	}
+
+	err = cfg.SetUser("simon")
+	if err != nil {
+		fmt.Println("Error setting user:", err)
+		return
+	}
+
+	updatedCfg, err := config.Read()
+	if err != nil {
+		fmt.Println("Error reading updated config:", err)
+		return
+	}
+
+	fmt.Printf("Config: %+v,\n", updatedCfg)
+}
